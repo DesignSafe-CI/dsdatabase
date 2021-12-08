c2857e60d4ba0c89c931cf4fdf68f8393c4cf5ae
sjbrandenberg <sjbrandenberg@ucla.edu>
2021-12-08 07:39:58 -0800

    Create setup.py

---
+++ ngl_db/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ngl_db',
+      version='0.1',
+      description='ngl_db connection',
+      url='',
+      author='Scott J. Brandenberg',
+      author_email='sjbrandenberg@ucla.edu',
+      license='MIT',
+      packages=['ngl_db'],
+      zip_safe=False)
