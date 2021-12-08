2a21034475368cce0b273577f78ecc4124bd3be4
sjbrandenberg <sjbrandenberg@ucla.edu>
2021-12-08 07:45:02 -0800

    Create setup.py

---
+++ earthquake_recovery_db/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='earthquake_recovery_db',
+      version='0.1',
+      description='earthquake_recovery_db connection',
+      url='',
+      author='Scott J. Brandenberg',
+      author_email='sjbrandenberg@ucla.edu',
+      license='MIT',
+      packages=['earthquake_recovery_db'],
+      zip_safe=False)
