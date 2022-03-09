4954a1a979ef33ac8436c65a501bf4f55ccdeb6b
sjbrandenberg <sjbrandenberg@ucla.edu>
2022-03-09 12:55:16 -0800

    Update __init__.py
    Updated to use sqlalchemy to avoid Pandas query warnings

--- ngl_db/ngl_db/__init__.py
+++ ngl_db/ngl_db/__init__.py
@@ -1,11 +1,10 @@
-# Establishes connection to NGL database from MySQL Workbench
 import pymysql
 import pandas as pd
+import sqlalchemy
 
 def connect():
     try:
-        print('We will be phasing out the ngl_db.connect() command in favor of the ngl_db.read_sql(sql, output) command in the future. Please see https://ngl-tools.readthedocs.io/en/latest/ for details. If you use cnx = ngl_db.connect(), please close your connection [cnx.close()] when you are finished querying data. Open connections will be manually closed each night to facilitate database replication.')
-        return(pymysql.connect(user='dspublic', password='R3ad0nlY', host='129.114.52.174', port=3306, db='sjbrande_ngl_db', read_timeout=600.0))
+        print('We have phased out the ngl_db.connect() command in favor of the ngl_db.read_sql(sql, output) command. Please see https://ngl-tools.readthedocs.io/en/latest/ for details.')
     except:
         pass
     
@@ -13,25 +12,22 @@ def read_sql(**kwargs):
     if('sql' not in kwargs):
         print('You must specify sql= in your argument list')
         return
-    try:
-        cnx = pymysql.connect(user='dspublic', password='R3ad0nlY', host='129.114.52.174', port=3306, db='sjbrande_ngl_db')
-    except:
-        print('db connection failed')
-        pass
     if('output' in kwargs):
         output = kwargs['output']
     else:
         output = 'DataFrame'
     if(output=='DataFrame'):
         try:
-            data = pd.read_sql(sql, cnx)
-            cnx.close()
+            engine = sqlalchemy.create_engine('mysql+pymysql://dspublic:R3ad0nlY@129.114.52.174:3306/sjbrande_ngl_db')
+            data = pd.read_sql_query(sql, con=engine)
+            engine.dispose()
             return(data)
         except:
-            print('pd.read_sql(sql, cnx) failed')
+            print('pd.read_sql_query(sql, cnx) failed')
             pass
     elif(output=='dict'):
         try:
+            cnx = pymysql.connect(user='dspublic', password='R3ad0nlY', host='129.114.52.174', port=3306, db='sjbrande_ngl_db')
             cur = cnx.cursor(pymysql.cursors.DictCursor)
             cur.execute(sql)
             data = cur.fetchall()
