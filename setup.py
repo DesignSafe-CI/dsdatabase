4ef349e833f60a6dc9462b044dfd136f3212d46b
sjbrandenberg <sjbrandenberg@ucla.edu>
2022-03-09 13:15:10 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="designsafe_db",
+    version="0.0.1",
+    author="Scott J. Brandenberg",
+    author_email="sjbrandenberg@ucla.edu",
+    description="DesignSafe relational database connection scripts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sjbrandenberg/DesignSafe_db_scripts",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
