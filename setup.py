add7aa26bfba07beb6e8dcde4f3977de4e84b532
sjbrandenberg <sjbrandenberg@ucla.edu>
2022-03-09 16:32:15 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="designsafe_db",
-    version="0.0.2",
+    version="0.0.3",
     author="Scott J. Brandenberg",
     author_email="sjbrandenberg@ucla.edu",
     description="DesignSafe relational database connection scripts",
