c0f5d3353e278bef9f58f6d9558e63b9c7e1a1c4
sjbrandenberg <sjbrandenberg@ucla.edu>
2022-03-14 15:11:37 -0700

    Update vp_db.py
    Updating to use SQLAlchemy and error reporting

--- designsafe_db/vp_db.py
+++ designsafe_db/vp_db.py
@@ -1,37 +1,49 @@
 # Establishes connection to NGL database from MySQL Workbench
 import pymysql
 import pandas as pd
+import sqlalchemy
+from sqlalchemy import exc
 
 def connect():
     try:
-        print('We will be phasing out the vp_db.connect() command in favor of the vp_db.read_sql(sql, output) command in the future. If you use cnx = vp_db.connect(), please close your connection [cnx.close()] when you are finished querying data. Open connections will be manually closed each night to facilitate database replication.')
-        return(pymysql.connect(user='dspublic', password='R3ad0nlY', host='129.114.52.174', port=3306, db='sjbrande_vpdb', read_timeout=600.0))
+        print('We have phased out the vp_db.connect() command in favor of the vp_db.read_sql(sql, output) command.')
     except:
         pass
     
-def read_sql(sql, output):
-    try:
-        cnx = pymysql.connect(user='dspublic', password='R3ad0nlY', host='129.114.52.174', port=3306, db='sjbrande_vpdb')
-    except:
-        print('db connection failed')
-        pass
+def read_sql(*args):
+    if(len(args)<1):
+        print('You must specify a sql string in the read_sql function')
+        return
+    if(len(args)>2):
+        print('You must only specify sql and type in the read_sql function')
+        return
+    sql = args[0]
+    if(len(args)==1):
+        output = 'DataFrame'
+    else:
+        if(args[1] in ['DataFrame','dict']):
+            output = args[1]
+        else:
+            print('type "' + str(args[1]) + '" is not a valid option. Must be either "DataFrame" or "dict". Using "DataFrame" type as default.')
+            
     if(output=='DataFrame'):
         try:
-            data = pd.read_sql(sql, cnx)
-            cnx.close()
+            engine = sqlalchemy.create_engine('mysql+pymysql://dspublic:R3ad0nlY@129.114.52.174:3306/sjbrande_vpdb')
+            data = pd.read_sql_query(sql, con=engine)
+            engine.dispose()
             return(data)
-        except:
-            print('pd.read_sql(sql, cnx) failed')
+        except exc.SQLAlchemyError as e:
+            print(e)
             pass
     elif(output=='dict'):
-        try:
+        cnx = pymysql.connect(user='dspublic', password='R3ad0nlY', host='129.114.52.174', port=3306, db='sjbrande_vpdb')
             cur = cnx.cursor(pymysql.cursors.DictCursor)
             cur.execute(sql)
             data = cur.fetchall()
             cnx.close()
             return(data)
-        except:
-            print('cur.fetchall() failed')
+        except pymysql.Error as e:
+            print("Error %d: %s" % (e.args[0], e.args[1]))
             pass
     else:
         print('In vp_db.read_sql(sql, output), output must be either "DataFrame" or "dict", not "' + output + '"')
